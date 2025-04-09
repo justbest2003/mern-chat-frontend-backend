82f82a9ccb53a19c0a77efc14e4c384c64b11cab
Justl3est <111554501+justbest2003@users.noreply.github.com>
2025-04-09 12:19:12 +0700

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -13,7 +13,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 24 * 60 * 60 * 1000, //MS
     httpOnly: true, //XSS Attacks
-    sameSite: "lax", //CSRF attacks
+    sameSite: "none", //CSRF attacks
     secure: node_mode !== "development", //HTTPS only
   });
 
